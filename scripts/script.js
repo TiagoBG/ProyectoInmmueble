524c8fe433746296910e2498633ff31f229f19db
Santiago Betancur Graciano <santybg_94@hotmail.com>
2020-11-11 20:07:44 -0500

    Se suben los cambios de la clase del 11-11

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,21 @@
-document.getElementById('btn-guardar-mensualidad').addEventListener("click", function() {
-    let a = 4;
-    let b = 6;
-    alert('La suma es ' + (a + b));
+document.querySelector('#btn-guardar-mensualidad').addEventListener("click", e => {
+    if (document.querySelector('#frm-mensualidad').reportValidity()) {
+        let name = document.querySelector('#nombre_propietario').value
+        let docNum = document.querySelector('#doc_propietario').value
+        let names = new Array("Tiago", "Andrea", "Cristian")
+        names.push(name)
+        console.log(`ID: ${docNum}`)
+        console.log(`Registros: ${names}`)
+
+    }
 });
+
+let guardarMensualidad = event => {
+    let a, b, sum
+    a = 3
+    b = 5
+    sum = a + b
+    console.log(event);
+    console.log("La suma de a y b es " + sum);
+    return sum
+}
